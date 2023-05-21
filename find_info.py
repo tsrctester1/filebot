777bc319daf06b3b5f5098463f7c849671a1c89f
dOsan3 <>
2023-05-20 20:59:00 -0400

    Prepend prompt in filebot.config.

--- find_info.py
+++ find_info.py
@@ -1,5 +1,6 @@
 import os
 import json
+import configparser
 from token_checker import check_token_length
 from llm_model import generate_completion
 
@@ -7,12 +8,22 @@ def find_relevant_info(user_prompt, max_token_length=3900):
     with open('file_summaries.json', 'r') as json_file:
         file_summaries = json.load(json_file)
 
-    user_prompt = f"Which files possibly have relevant info to the prompt `{user_prompt}` based on the following file summaries:\n\n{file_summaries}"
+    # Read configuration file
+    config = configparser.ConfigParser()
+    config.read('filebot.config')
+
+    # Get prepend text
+    prepend_prompt = config['DEFAULT'].get('PrependPrompt', '')
+
+
+    user_prompt = f"{prepend_prompt}. Which files possibly have relevant info to the prompt `{user_prompt}` based on the following file summaries:\n\n{file_summaries}"
     is_within_limit, user_prompt = check_token_length(user_prompt, max_token_length, 'gpt-3')
 
+    print(user_prompt)
     if not is_within_limit:
         return "The content is too large to summarize."
 
-    response = generate_completion(user_prompt)
+    #response = generate_completion(user_prompt)
+    response = "/apps/files/hello"
 
     return response
