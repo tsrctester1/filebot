f0ace6357b36ba038a765187493c71756bc0bd77
dOsan3 <>
2023-06-02 09:36:20 -0400

    Add colors to cli.

--- filebot.py
+++ filebot.py
@@ -8,7 +8,7 @@ from find_info import answer_prompt
 def answer_user_prompt(relevant_info):
     """Generate a response to the user's prompt based on the relevant info."""
     if not relevant_info:
-        return "I'm sorry, I couldn't find any files that contain the term '{}'. Please try another search.".format(user_prompt)
+        return "I'm sorry, I couldn't find any relevant files to answer your question. Please rephrase or try another search."
     else:
         response = "Answer: {}".format(relevant_info)
         return response
@@ -25,24 +25,26 @@ def main():
     directory = '/app/files'
     create_file_summaries(directory)
 
-    user_prompt = input("Prompt: ")
-    relevant_info = find_relevant_info(user_prompt)
+    while True:
+        user_prompt = input("\033[92mPrompt:\033[0m ")
+        relevant_info = find_relevant_info(user_prompt)
 
-    response = answer_user_prompt(relevant_info)
+        response = answer_user_prompt(relevant_info)
 
-    # Extract file paths
-    file_paths = extract_file_paths(response)
+        # Extract file paths
+        file_paths = extract_file_paths(response)
 
-    if file_paths:
-        # Print file paths
-        for file in file_paths:
-            stripped_file_path = re.sub(r'\.\d+$', '', file)
-            answer = answer_prompt(stripped_file_path, user_prompt, answer_type='final_answer')
-            print(f"\n{answer}")
-            print(f"\nsource: {stripped_file_path}")
-            break
-    else:
-        print("No files found")
+        if file_paths:
+            # Print file paths
+            for file in file_paths:
+                stripped_file_path = re.sub(r'\.\d+$', '', file)
+                answer = answer_prompt(stripped_file_path, user_prompt, answer_type='final_answer')
+                print(f"\n\n{answer}")
+                print(f"\033[1;97m\nsource: {stripped_file_path}\033[0m")
+                print("\n\n")
+                break
+        else:
+            print("\033[38;5;208mNo files found\033[0m")
 
 if __name__ == '__main__':
     main()
